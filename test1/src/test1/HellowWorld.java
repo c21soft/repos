29bf145dcd906b7765c2ab8b8dc2ac5f1c84ebb4
ryu <ryu@ryu-PC>
2018-12-30 21:13:24 +0900

    수정4커밋

--- test1/src/test1/HellowWorld.java
+++ test1/src/test1/HellowWorld.java
@@ -5,6 +5,7 @@ public class HellowWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요. Hellow World");	
+		System.out.println("수정4 커밋합니다.");	
 	}
 
 }
