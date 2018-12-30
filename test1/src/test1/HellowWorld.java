7e9a8184c74b5c4fd592e38e61fe18762b3eb509
ryu <ryu@ryu-PC>
2018-12-30 20:27:20 +0900

    수정2

--- test1/src/test1/HellowWorld.java
+++ test1/src/test1/HellowWorld.java
@@ -5,7 +5,7 @@ public class HellowWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("안녕하세요. Hellow World");	
-		System.out.println("안녕하세요. 수정합니다.");	
+		System.out.println("수정2 커밋합니다.");
 	}
 
 }
